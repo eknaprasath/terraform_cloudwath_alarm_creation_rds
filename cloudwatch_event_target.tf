93b64dfa749a29f0eeb6330b7d5f6da89daf3834
Eknaprasath Padmaraj <EP00606284@techmahindra.com>
2020-09-03 13:05:23 +0530

    added cw enent

--- cloudwatch_event_target.tf
+++ cloudwatch_event_target.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_event_target" "CW-Target" {
   target_id = "CW-Alarm-Creation-RDS"
-  rule      = aws_cloudwatch_event_rule.console.name
+  rule      = aws_cloudwatch_event_rule.RDS-Alarm-Creation.name
   arn       = aws_lambda_function.CW-Alarm-Creation-RDS.arn
 }
 
